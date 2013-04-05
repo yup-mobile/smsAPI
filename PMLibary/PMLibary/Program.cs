674bce6139f095b280ff6f1ad4c723f2d2ad7b54
邮差 <sunwj@yup-mobile.com>
2013-04-05 10:40:44 +0800

    update ios .net

---
+++ PMLibary/PMLibary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace PMLibary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostManAPI.sendsms("zhengyp", "12345678aA!", "恭喜发财", "13917980980", "", "");
+        }
+    }
+}
---
+++ PMLibary/PMLibary/WebSrvCaller.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Web;
+using System.Xml;
+using System.Collections;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace PMLibary
+{
+    class WebSrvCaller
+    {
+        private static Hashtable _xmlNamespaces = new Hashtable();
+
+        public static XmlDocument QuerySoapWebService(string soapURL, string MethodName, Queue Pars, string WSDLURL, string soapBody = "")
+        {
+            string XmlNs = null;
+            if (_xmlNamespaces.ContainsKey(soapURL))
+            {
+                XmlNs = _xmlNamespaces[soapURL].ToString();
+            }
+            else
+            {
+                XmlNs = GetNamespace(WSDLURL);
+                _xmlNamespaces[soapURL] = XmlNs;
+            }
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(soapURL);
+            request.Method = "POST";
+            request.ContentType = "text/xml; charset=utf-8";
+            //request.Headers.Add("SOAPAction", "\"" + "urn:" + MethodName + "\"");
+            SetWebRequest(request);
+            byte[] data;
+            if (soapBody == "")
+            {
+                data = EncodeParsToSoap(Pars, XmlNs, MethodName);
+            }
+            else
+            {
+                data = Encoding.UTF8.GetBytes(soapBody);
+            }
+            
+            WriteRequestData(request, data);
+            XmlDocument doc = new XmlDocument(), doc2 = new XmlDocument();
+            doc = ReadXmlResponse(request.GetResponse());
+
+            XmlNamespaceManager mgr = new XmlNamespaceManager(doc.NameTable);
+            mgr.AddNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/");
+            String RetXml = doc.SelectSingleNode("//soap:Body/*/*", mgr).InnerXml;
+            doc2.LoadXml("<root>" + RetXml + "</root>");
+            AddDelaration(doc2);
+            return doc2;
+        
+        }
+        private static void WriteRequestData(HttpWebRequest request, byte[] data)
+        {
+            request.ContentLength = data.Length;
+            Stream writer = request.GetRequestStream();
+            writer.Write(data, 0, data.Length);
+            writer.Close();
+        }
+
+        private static XmlDocument ReadXmlResponse(WebResponse response)
+        {
+            StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            String retXml = sr.ReadToEnd();
+            sr.Close();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(retXml);
+            return doc;
+        }
+
+        private static string GetNamespace(String URL)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            SetWebRequest(request);
+            WebResponse response = request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(sr.ReadToEnd());
+            sr.Close();
+            return doc.SelectSingleNode("//@targetNamespace").Value;
+        }
+
+        private static void SetWebRequest(HttpWebRequest request)
+        {
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Timeout = 10000;
+        }
+
+        private static byte[] EncodeParsToSoap(Queue Pars, String XmlNs, String MethodName)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"></soap:Envelope>");
+            AddDelaration(doc);
+            XmlElement soapBody = doc.CreateElement("soap", "Body", "http://schemas.xmlsoap.org/soap/envelope/");
+            XmlElement soapMethod = doc.CreateElement(MethodName);
+            soapMethod.SetAttribute("xmlns", XmlNs);
+            foreach (DictionaryEntry par in Pars)
+            {
+                string parName = (string)par.Key;
+                string parValue = (string)par.Value;
+                XmlElement soapPar = doc.CreateElement(parName);
+                soapPar.InnerXml = ObjectToSoapXml(parValue);
+                soapMethod.AppendChild(soapPar);
+            }
+
+         /*   foreach (string k in Pars.Keys)
+            {
+                XmlElement soapPar = doc.CreateElement(k);
+                soapPar.InnerXml = ObjectToSoapXml(Pars[k]);
+                soapMethod.AppendChild(soapPar);
+            }*/
+            soapBody.AppendChild(soapMethod);
+            doc.DocumentElement.AppendChild(soapBody);
+            return Encoding.UTF8.GetBytes(doc.OuterXml);
+        }
+
+        private static void AddDelaration(XmlDocument doc)
+        {
+            XmlDeclaration decl = doc.CreateXmlDeclaration("1.0", "utf-8", null);
+            doc.InsertBefore(decl, doc.DocumentElement);
+        }
+
+        private static String ParsToString(Hashtable Pars)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string k in Pars.Keys)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append("&");
+                }
+                sb.Append(HttpUtility.UrlEncode(k) + "=" + HttpUtility.UrlEncode(Pars[k].ToString()));
+            }
+            return sb.ToString();
+        }
+
+        private static string ObjectToSoapXml(object o)
+        {
+            XmlSerializer mySerializer = new XmlSerializer(o.GetType());
+            MemoryStream ms = new MemoryStream();
+            mySerializer.Serialize(ms, o);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(Encoding.UTF8.GetString(ms.ToArray()));
+            if (doc.DocumentElement != null)
+            {
+                return doc.DocumentElement.InnerXml;
+            }
+            else
+            {
+                return o.ToString();
+            }
+        }
+        private static byte[] EncodePars(Hashtable Pars)
+        {
+            return Encoding.UTF8.GetBytes(ParsToString(Pars));
+        }
+    }
+}
